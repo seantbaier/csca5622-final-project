46f4859fc1ba2f4de9d8634e6f0a1f189d126027
Sean Baier <sean.t.baier@gmail.com>
2024-10-15 05:25:39 -0500

    move col mappings

--- src/csca5622_final_project/__init__.py
+++ src/csca5622_final_project/__init__.py
@@ -1,1 +1,12 @@
+from .column_mappings import (
+    TEACHER_CREDENTIALS_COLUMN_MAPPINGS,
+    PRESCHOOL_ENROLLMENT_COLUMN_MAPPINGS,
+)
 
+from .utils import load_excel
+
+__all__ = [
+    "load_excel",
+    "TEACHER_CREDENTIALS_COLUMN_MAPPINGS",
+    "PRESCHOOL_ENROLLMENT_COLUMN_MAPPINGS",
+]
---
+++ src/csca5622_final_project/column_mappings.py
@@ -0,0 +1,59 @@
+TEACHER_CREDENTIALS_COLUMN_MAPPINGS = [
+    "C0",
+    "State",
+    "CT (FTE)",
+    # "Classroom Teachers (FTE)",
+    "MR (FTE)",
+    # "Meeting All State Licensing/Certification Requirements (FTE)",
+    "MR (P)",
+    # "Meeting All State Licensing/Certification Requirements (P)",
+    "FY (FTE)",
+    # "Classroom Teachers in their First Year of Teaching (FTE)",
+    "FY (P)",
+    # "Classroom Teachers in their First Year of Teaching (P)",
+    "SY (FTE)",
+    "SY (P)",
+    "schools",
+    "schools (P)",
+]
+
+PRESCHOOL_ENROLLMENT_COLUMN_MAPPINGS = [
+    "C0",
+    "State",
+    "Total (N)",
+    "Total (P)",
+    # "American Indian or Alaska Native (N)",
+    "AIAN (N)",
+    # "American Indian or Alaska Native (P)",
+    "AIAN (P)",
+    "Asian (N)",
+    "Asian (P)",
+    # "Hispanic or Latino of any race (N)",
+    "HL (N)",
+    # "Hispanic or Latino of any race (P)",
+    "HL (P)",
+    # "Black or African American (N)"
+    "BAA (N)",
+    # "Black or African American (P)"
+    "BAA (N)",
+    "White (N)",
+    "White (P)",
+    # "Native Hawaiian or Other Pacific Islander (N)"
+    "NHPI (N)",
+    # "Native Hawaiian or Other Pacific Islander (P)"
+    "NHPI (P)",
+    # "Two or more races (N)",
+    "TMR (N)",
+    # "Two or more races (P)",
+    "TMR (P)",
+    # Students With Disabilities Served Under IDEA
+    "SWDSUI (N)",
+    "SWDSUI (P)",
+    # English Language Learners
+    "ELL (N)",
+    "ELL (P)",
+    # Number of Schools
+    "schools",
+    # Percent of Schools Reporting
+    "schools (P)",
+]
